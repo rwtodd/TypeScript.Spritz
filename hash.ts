092880c9b2838c7679a65a68c6f08679c46a32ae
Richard Todd <rwtodd@users.noreply.github.com>
2017-05-23 14:49:20 -0500

    added a hash of arrays

--- hash.ts
+++ hash.ts
@@ -3,12 +3,19 @@
 
 namespace Spritz {
 
-	export function hashString(tgt: Uint8Array, src: string) {
+	function hashCipher(tgt: Uint8Array, ciph: Cipher) : void {
+		ciph.absorbStop()
+		ciph.absorbNumber(tgt.length)
+		ciph.squeeze(tgt)
+	}
+	export function hashString(tgt: Uint8Array, src: string) : void {
 		var cipher = new Spritz.Cipher()
 		cipher.soak(Util.encodeUTF8(src))
-		cipher.absorbStop()
-		cipher.absorbNumber(tgt.length)
-		cipher.squeeze(tgt)
+		hashCipher(tgt, cipher)
 	}
-
+	export function hashArray(tgt: Uint8Array, src: Uint8Array) : void {
+		var cipher = new Spritz.Cipher()
+		cipher.soak(src)
+		hashCipher(tgt, cipher)
+	}
 }
